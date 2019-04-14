1653739a242755790f2329fb790ac994d0f50e8d
katglin <katerinaglinskaya96@gmail.com>
2019-04-14 19:55:15 +0300

    dashboard -> home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { StickersComponent }      from './components/stickers/stickers.component
 import { StickerDetailComponent }  from './components/sticker-detail/sticker-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: StickersComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: StickersComponent },
   { path: 'detail/:id', component: StickerDetailComponent }
 ];
 
